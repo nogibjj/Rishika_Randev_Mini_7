5d394f56c682b6e79e15175118eee9b8c8f1781c
Rishika Randev <rish.ran12@gmail.com>
2024-10-02 14:31:16 -0400

    Extract file

--- mylib/extract.py
+++ mylib/extract.py
@@ -5,8 +5,8 @@ food dataset
 """
 import requests
 
-def extract(url="https://raw.githubusercontent.com/Barabasi-Lab/GroceryDB/main/data/GroceryDB_IgFPro.csv", 
-            file_path="data/GroceryDB_IgFPro.csv"):
+def extract(url="https://data.cdc.gov/api/views/hn4x-zwk7/rows.csv?accessType=DOWNLOAD", 
+            file_path="data/Behaviors.csv"):
     """"Extract a url to a file path"""
     with requests.get(url) as r:
         with open(file_path, 'wb') as f:

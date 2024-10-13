092f0db52493f4ae11f9050d338488b01f93ba71
Rishika Randev <rish.ran12@gmail.com>
2024-10-13 00:54:40 -0400

    Moving around files

--- test_main.py
+++ test_main.py
@@ -3,9 +3,9 @@ Test goes here
 
 """
 
-from src.mylib.extract import extract
-from src.mylib.transform_load import load
-from src.mylib.query import query
+from mylib.extract import extract
+from mylib.transform_load import load
+from mylib.query import query
 
 
 def test_extract():
